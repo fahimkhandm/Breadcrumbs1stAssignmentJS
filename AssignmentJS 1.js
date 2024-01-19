54f3a34d6f7c0fee5ed8a2c78c0699eec0e59c2a
fahimkhandm <155655412+fahimkhandm@users.noreply.github.com>
2024-01-19 22:32:40 +0600

    Add files via upload

---
+++ AssignmentJS 1.js
@@ -0,0 +1,38 @@
+// ১. Variable কি ?
+variable is a named storage location that can hold data values.
+// ২. Variable কিভাবে লিখতে হয় ?
+Variable write useing java Reserved keyword "var" 
+like: var myFriendnmae="Tahmin"
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+. Strings are created by enclosing text within single (') or double (") quotes. 
+like: Var MyName="fahim"
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+Number type variable is number data type is used to represent numeric values.
+like: var myClassRoll=5
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+Boolean type variable it can have only two possible values: true or false.
+like: var Islam="true";
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+toUpperCase() and toLowerCase() are methods that can be applied to strings. They are used to convert the characters of a string to uppercase or lowercase, respectively.
+like: var rocona="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget justo in libero gravida bibendum. Suspendisse potenti. Integer nec fringilla libero. Phasellus nec convallis odio. Curabitur non consequat turpis. Integer auctor nisi in mauris lobortis, at fermentum libero fringilla. Sed sagittis, justo vitae feugiat sodales, nisi dolor "
+console.log(rocona.toLocaleUpperCase())
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+java have 4 Operators
++ (Addition)
+- (Subtraction)
+* (Multiplication)
+/ (Division)
+% (Modulus, returns the remainder)
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+when we can change java negative values
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+In Java, the Math.ceil() method is used to round a floating-point number up to the nearest integer. 
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+math Floor "floor" refers to the concept of finding the largest integer that is less than or equal to the given number.
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+The Math.round() method in Java is used to round a floating-point number to the nearest integer.  If the fractional part of the argument is exactly 0.5, the method rounds towards the even number.
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+Math.random  generates a random integer between 0 (inclusive) and 10 (exclusive).
